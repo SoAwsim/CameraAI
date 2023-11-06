905a5ceb84d1d8c5aeda1e279c832cf4b00dc8a0
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2023-11-06 18:36:36 +0100

    Add navigation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,5 +67,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // Navigation
+    val nav_version = "2.7.5"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 
 }
